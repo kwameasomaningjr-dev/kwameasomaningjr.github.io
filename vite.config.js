ecaa9757e36a5858122e48563db003dd3337ae2a
kwameasomaningjr-dev <kwameasomaningjr@gmail.com>
2026-02-14 17:46:38 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // This is correct for your specific username.github.io repo
+  base: "/", 
 })
